f7801470eb92a5466bbaabba3fa3907e7b138986
loma <loma.attree@humanfractal.io>
2022-04-05 12:38:10 +0530

    Application class and hilt dependancy added

---
+++ app/src/main/java/com/example/coffee_themesandtypography/Drink_Coffee_Application.kt
@@ -0,0 +1,9 @@
+package com.example.coffee_themesandtypography
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Drink_Coffee_Application :Application()
+
+
